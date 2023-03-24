19bb09f6608862592da46f5f9da9eee12d26249d
PatGal <paterson.nya@gmail.com>
2023-03-24 11:27:30 +0300

    Teste If Switch etc

--- js/app.js
+++ js/app.js
@@ -1,5 +1,49 @@
 
-const isDoor = true;
-if (isDoor) {
-  console.log('Bienvenue');  
-}
+// const isDoor = true;
+// if (isDoor) {
+//   console.log('Bienvenue');  
+// } else {
+//   console.log('Appele ton ami');
+// }
+
+// const color = 'red';
+
+// if (color === 'red') {
+//   console.log('Stop');
+// } else if (color === 'yellow') {
+//   console.log('Attention');
+// } else if (color === 'green') {
+//   console.log('Rouler');
+// } else {
+//   console.log('Feux gate');
+// }
+
+// Entre pour adulte
+// const userAge = '';
+// if (userAge < 18) {
+//   console.log('Entre inderdite');
+// } else if (userAge >= 18) {
+//   console.log('Bienvenue');
+// } else if (Number(userAge) != false) {
+//   console.log('Entrez votre age');
+// } 
+// const color = 'green';
+// const message = color === 'red'? 'Stop' : color === 'greeen'? 'Rouler' : color === 'yellow' ? 'Attention' : 'Feux gate';
+// console.log(message);
+ //LA meme chose avec switch
+
+ const color = '';
+ switch (color) {
+  case 'red':
+    console.log('Stop');
+    break;
+  case 'green':
+    console.log('Roulez');
+    break;
+  case 'yellow':
+    console.log('Attention');
+    break;
+  default:
+    console.log('Feux en panne');
+    break;
+ }
--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,5 +1,8 @@
-
-const isDoor = true;
-if (isDoor) {
-  console.log('Bienvenue');  
+const userAge = '';
+if (userAge < 18) {
+  console.log('Entre inderdite');
+} else if (userAge >= 18) {
+  console.log('Bienvenue');
+} else if (Number(userAge) != false) {
+  console.log('Entrez votre age');
 }
